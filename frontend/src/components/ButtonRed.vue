16663e64cc34ac9594629ef5a0a53414fb864597
Diego Oliveira <diego263526352635@gmail.com>
2025-01-15 20:08:35 -0300

    Added ButtonRed Component

---
+++ frontend/src/components/ButtonRed.vue
@@ -0,0 +1,27 @@
+<template>
+    <input type="button" :value=label>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: ['label'],
+    };
+</script>
+
+
+<style scoped>
+
+input{
+    height:60px;
+    border-radius: 15px;
+    border-style: none;
+    background-color: #F3191D;
+    color: white;
+    font-size:3em;
+}
+
+input:hover{
+    background-color: #C6191C;
+}
+</style>
