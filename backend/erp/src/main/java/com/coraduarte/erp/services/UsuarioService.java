8d0f51895391a201fac38bc7cc299ad065d3fef1
dirgogo <diego263526352635@gmail.com>
2025-02-05 17:46:15 -0300

    Updated added PasswordEncryption

--- backend/erp/src/main/java/com/coraduarte/erp/services/UsuarioService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/UsuarioService.java
@@ -1,18 +1,25 @@
 package com.coraduarte.erp.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import com.coraduarte.erp.models.Usuario;
+import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.UsuarioRepository;
 
 public class UsuarioService {
     
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Usuario findById(Long id){
     Optional<Usuario> usuario = this.usuarioRepository.findById(id);
@@ -24,17 +31,19 @@ public class UsuarioService {
    @Transactional
    public Usuario create(Usuario obj){
     obj.setId(null);
+    obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+    obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
     obj = this.usuarioRepository.save(obj);
     return obj;
    }
 
-   @Transactional
+   /*  @Transactional
    public Usuario update(Usuario obj){
      Usuario newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
         return this.usuarioRepository.save(newObj);
    }
-
+*/
    public void delete(Long id){
     findById(id);
       try {
