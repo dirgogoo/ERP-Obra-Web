a2c7cb73e68e24a91a5e430be5ec7326e1baa802
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-05 17:08:45 -0300

    Create ObraRepository

--- backend/erp/src/main/java/com/coraduarte/erp/repositories/ObraRepository.java
+++ backend/erp/src/main/java/com/coraduarte/erp/repositories/ObraRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coraduarte.erp.models.Obras;
 
-public interface ObraRepository extends JpaRepository<Obras, Long> {
+public interface ObraRepository extends JpaRepository<Obras, Long>  {
     
 }
