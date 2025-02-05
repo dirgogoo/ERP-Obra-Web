54fdefc5a4653801ddd086816c9a75a95a754cc7
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-05 17:06:34 -0300

    Alteraçao do nome de MateriaisRepos para MaterialRepos

--- backend/erp/src/main/java/com/coraduarte/erp/repositories/MaterialRepository.java
+++ backend/erp/src/main/java/com/coraduarte/erp/repositories/MaterialRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coraduarte.erp.models.Materiais;
 
-public interface MateriaisRepository  extends JpaRepository<Materiais, Long> {
+public interface MaterialRepository  extends JpaRepository<Materiais, Long> {
     
 
 }
