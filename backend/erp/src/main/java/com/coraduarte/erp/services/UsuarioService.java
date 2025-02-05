c0f65fa206e67aa36672f600446fae46dc354ca9
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-05 17:17:39 -0300

    Create UsuarioService

--- backend/erp/src/main/java/com/coraduarte/erp/services/UsuarioService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/UsuarioService.java
@@ -40,7 +40,7 @@ public class UsuarioService {
       try {
           this.usuarioRepository.deleteById(id);
       } catch (Exception e) {
-         throw new RuntimeException("Não é possível excluir este usuário!");
+         throw new RuntimeException("Não é possível excluir este usuário!" );
       }
    }
 }
