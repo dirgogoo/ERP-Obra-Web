d7663ecb8abd4261924e4dfb7cd6fe2d71271ca4
dirgogo <diego263526352635@gmail.com>
2025-02-05 17:45:19 -0300

    Refactor User to ProfileEnum

--- backend/erp/src/main/java/com/coraduarte/erp/models/Usuario.java
+++ backend/erp/src/main/java/com/coraduarte/erp/models/Usuario.java
@@ -1,7 +1,21 @@
 package com.coraduarte.erp.models;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.context.annotation.Profile;
+
+import com.coraduarte.erp.models.enums.ProfileEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,5 +52,21 @@ public class Usuario {
     @Column(name = "password", nullable = false)
     @NotNull
     @NotEmpty
+    @JsonProperty(access =  JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access =  JsonProperty.Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name="profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+    public Set<ProfileEnum> getProfiles(){
+        return this.profiles.stream().map(x -> ProfileEnum.toProfileEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(ProfileEnum profileEnum){
+        this.profiles.add(profileEnum.getCode());
+    }
+
 }
