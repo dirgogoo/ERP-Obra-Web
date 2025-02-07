e7854518764d61793900bed96d402942d3fc20e7
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-07 14:25:43 -0300

    Change from Etapas to Etapa

---
+++ backend/erp/src/main/java/com/coraduarte/erp/models/Etapa.java
@@ -0,0 +1,50 @@
+package com.coraduarte.erp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Column;
+
+
+@Entity
+@Table(name = Etapa.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+
+public class Etapa {
+    public static final String TABLE_NAME = "stages";
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", unique = true )
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    @NotNull
+    @NotEmpty
+    private String name;
+
+
+    @Column(name = "price", nullable = false)
+    @NotNull
+    @NotEmpty
+    private Long price;
+
+    @Column(name = "deadline", nullable = false)
+    @NotNull
+    @NotEmpty
+    private Integer deadline;
+
+}
