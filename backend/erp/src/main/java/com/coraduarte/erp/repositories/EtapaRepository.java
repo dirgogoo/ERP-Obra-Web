859b68a45a7090cc1ea4d07f5a095fe09a113724
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-07 14:28:57 -0300

    Change from Etapas to Etapa in EtapaRepository

---
+++ backend/erp/src/main/java/com/coraduarte/erp/repositories/EtapaRepository.java
@@ -0,0 +1,11 @@
+package com.coraduarte.erp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coraduarte.erp.models.Etapa;
+
+@Repository
+public interface EtapaRepository extends JpaRepository<Etapa, Long> {
+    
+}
