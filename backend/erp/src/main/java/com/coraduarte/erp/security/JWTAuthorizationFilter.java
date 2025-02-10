ecb69ded26b8f6f14c4f4cec7d38ec6dbca619f5
Diego Oliveira <diego263526352635@gmail.com>
2025-02-10 11:43:29 -0300

    Added JWTAuthorizationFilter

---
+++ backend/erp/src/main/java/com/coraduarte/erp/security/JWTAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.coraduarte.erp.security;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private JWTUtil jWTUtil;
+    private UserDetailsService userDetailsService;
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UserDetailsService userDetailsService){
+        super(authenticationManager);
+        this.jWTUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws  IOException, ServletException{
+         String authorizationHeader = request.getHeader("Authorization");
+         if(Objects.nonNull(authorizationHeader) && authorizationHeader.startsWith("Bearer ")){
+            String token = authorizationHeader.substring(7);
+            UsernamePasswordAuthenticationToken auth = getAuthentication(token);
+            if(Objects.nonNull(auth))
+            SecurityContextHolder.getContext().setAuthentication(auth);
+         }
+         filterChain.doFilter(request, response);
+    }
+
+
+    private UsernamePasswordAuthenticationToken getAuthentication(String token){
+        if(jWTUtil.isValidToken(token)){
+            String username = jWTUtil.getUsername(token);
+            UserDetails user = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            return authenticatedUser;
+        }
+        return null;
+    }
+
+}
