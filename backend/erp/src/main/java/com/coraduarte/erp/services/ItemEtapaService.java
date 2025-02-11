cb33f21689751524edf47897c1e7283db6dfe471
Diego Oliveira <diego263526352635@gmail.com>
2025-02-11 12:27:37 -0300

    Added Auth ItemEtapaService

--- backend/erp/src/main/java/com/coraduarte/erp/services/ItemEtapaService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/ItemEtapaService.java
@@ -1,33 +1,55 @@
 package com.coraduarte.erp.services;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coraduarte.erp.models.ItemEtapa;
+import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.ItemEtapaRepository;
+import com.coraduarte.erp.security.UserSpringSecurity;
 
 public class ItemEtapaService {
-    
+
     @Autowired
     private ItemEtapaRepository itemEtapaRepository;
 
-    public ItemEtapa findById(Long id){
+    public ItemEtapa findById(Long id) {
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity)) {
+            throw new AuthorizationDeniedException("Acesso negado!");
+        }
+
         Optional<ItemEtapa> itemEtapa = this.itemEtapaRepository.findById(id);
-        return itemEtapa.orElseThrow(()-> new RuntimeException(
-            "Item da etapa não encontrado!"
+        return itemEtapa.orElseThrow(() -> new RuntimeException(
+                "Item da etapa não encontrado!"
         ));
     }
 
     @Transactional
-    public ItemEtapa create(ItemEtapa obj){
+    public ItemEtapa create(ItemEtapa obj) {
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity) || !(userSpringSecurity.hasRole(ProfileEnum.ADMIN))) {
+            throw new AuthorizationDeniedException("Acesso negado!");
+        }
+
         obj.setId(null);
         obj = this.itemEtapaRepository.save(obj);
         return obj;
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity) || !(userSpringSecurity.hasRole(ProfileEnum.ADMIN))) {
+            throw new AuthorizationDeniedException("Acesso negado!");
+        }
+
         findById(id);
 
         try {
