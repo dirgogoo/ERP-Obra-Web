9a3f0ef4517f8b91a23d0dfdd5d2ac63765eb3cb
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-11 16:19:56 -0300

    Create ObraController

---
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/ObraController.java
@@ -0,0 +1,47 @@
+package com.coraduarte.erp.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.coraduarte.erp.models.Obra;
+import com.coraduarte.erp.models.User.CreateUser;
+import com.coraduarte.erp.services.ObraService;
+
+import jakarta.validation.Valid;
+
+public class ObraController {
+   
+    @Autowired
+    private ObraService obraService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Obra> findById(@PathVariable Long id){
+       Obra obj = this.obraService.findById(id);
+       return ResponseEntity.ok().body(obj);
+
+    }
+
+    @PostMapping
+    @Validated(CreateUser.class)
+    public ResponseEntity<Void> create(@Valid @RequestBody Obra obj){
+       this.obraService.create(obj);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+       return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.obraService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
