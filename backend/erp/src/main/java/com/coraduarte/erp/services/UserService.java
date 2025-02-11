c90e75969a350c51755c11b4e9574b1b85498c9b
Diego Oliveira <diego263526352635@gmail.com>
2025-02-10 21:37:44 -0300

    Defined Authorizations UserService

--- backend/erp/src/main/java/com/coraduarte/erp/services/UserService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/UserService.java
@@ -1,54 +1,73 @@
 package com.coraduarte.erp.services;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coraduarte.erp.models.User;
 import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.UserRepository;
+import com.coraduarte.erp.security.UserSpringSecurity;
 
 public class UserService {
-    
+
     @Autowired
     private UserRepository usuarioRepository;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User findById(Long id){
-    Optional<User> usuario = this.usuarioRepository.findById(id);
-        return usuario.orElseThrow(( )-> new RuntimeException(
-            "Usuário não encontrado! Id: " + id + ", Tipo:" + User.class.getName()
+    public User findById(Long id) {
+      
+         UserSpringSecurity authenticated = UserService.authenticated();
+
+        if (Objects.isNull(authenticated) && ( !( authenticated.hasRole(ProfileEnum.SUPERADMIN)) || ! (authenticated.getId().equals(id))) ) 
+            return null;
+        
+        
+        Optional<User> usuario = this.usuarioRepository.findById(id);
+        return usuario.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado! Id: " + id + ", Tipo:" + User.class.getName()
         ));
-   } 
+    }
 
-   @Transactional
-   public User create(User obj){
-    obj.setId(null);
-    obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
-    obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
-    obj = this.usuarioRepository.save(obj);
-    return obj;
-   }
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
+        obj = this.usuarioRepository.save(obj);
+        return obj;
+    }
 
-   /*  @Transactional
+    /*  @Transactional
    public Usuario update(Usuario obj){
      Usuario newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
         return this.usuarioRepository.save(newObj);
    }
-*/
-   public void delete(Long id){
-    findById(id);
-      try {
-          this.usuarioRepository.deleteById(id);
-      } catch (Exception e) {
-         throw new RuntimeException("Não é possível excluir este usuário!" );
-      }
-   }
+     */
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.usuarioRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir este usuário!");
+        }
+    }
+
+    public static UserSpringSecurity authenticated() {
+        try {
+            return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
