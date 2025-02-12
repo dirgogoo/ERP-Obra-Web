ce3d8803b34f7c649661467c80ee875cf1a104a7
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-12 17:55:11 -0300

    Change from Materiais to Material in MaterialService Refactor

--- backend/erp/src/main/java/com/coraduarte/erp/services/MaterialService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/MaterialService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.coraduarte.erp.models.Materiais;
+import com.coraduarte.erp.models.Material;
 import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.MaterialRepository;
 import com.coraduarte.erp.security.UserSpringSecurity;
@@ -17,21 +17,21 @@ public class MaterialService {
     @Autowired
     private MaterialRepository materialRepository;
 
-    public Materiais findById(Long id) {
+    public Material findById(Long id) {
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
 
         if (Objects.isNull(userSpringSecurity)) {
             throw new AuthorizationDeniedException("Acesso negado!");
         }
 
-        Optional<Materiais> material = this.materialRepository.findById(id);
+        Optional<Material> material = this.materialRepository.findById(id);
         return material.orElseThrow(() -> new RuntimeException(
-                "Material não encontrado! Id: " + id + ", Tipo:" + Materiais.class.getName()
+                "Material não encontrado! Id: " + id + ", Tipo:" + Material.class.getName()
         ));
     }
 
     @Transactional
-    public Materiais create(Materiais obj) {
+    public Material create(Material obj) {
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
 
         if (Objects.isNull(userSpringSecurity)) {
