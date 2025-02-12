f74e20a08b5921a7bd5f9716345c124d74612379
Diego Oliveira <diego263526352635@gmail.com>
2025-02-12 14:07:04 -0300

    Updated added FindAllByObraId EtapaObra

--- backend/erp/src/main/java/com/coraduarte/erp/repositories/EtapaObraRepository.java
+++ backend/erp/src/main/java/com/coraduarte/erp/repositories/EtapaObraRepository.java
@@ -1,11 +1,13 @@
 package com.coraduarte.erp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.coraduarte.erp.models.Etapa;
+import com.coraduarte.erp.models.EtapaObra;
 
 @Repository
-public interface EtapaObraRepository extends JpaRepository<Etapa, Long>{
-    
+public interface EtapaObraRepository extends JpaRepository<EtapaObra, Long>{
+    List<EtapaObra> findAllByObra_Id(Long id);
 }
--- backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
@@ -1,5 +1,6 @@
 package com.coraduarte.erp.services;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.coraduarte.erp.models.Etapa;
+import com.coraduarte.erp.models.EtapaObra;
 import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.EtapaObraRepository;
 import com.coraduarte.erp.security.UserSpringSecurity;
@@ -17,21 +18,34 @@ public class EtapaObraService {
     @Autowired
     private EtapaObraRepository etapaObraRepository;
 
-    public Etapa findById(Long id) {
+    public EtapaObra findById(Long id) {
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
 
         if (Objects.isNull(userSpringSecurity)) {
             throw new AuthorizationDeniedException("Acesso negado!");
         }
 
-        Optional<Etapa> etapa = this.etapaObraRepository.findById(id);
+        Optional<EtapaObra> etapa = this.etapaObraRepository.findById(id);
         return etapa.orElseThrow(() -> new RuntimeException(
                 "Etapa não encontrada!"
         ));
     }
 
+    public List<EtapaObra> findAllByObraId(Long id){
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity)) {
+            throw new AuthorizationDeniedException("Acesso negado!");
+        }
+
+        List<EtapaObra> etapas = this.etapaObraRepository.findAllByObra_Id(id);
+        return etapas;
+
+
+    }
+
     @Transactional
-    public Etapa create(Etapa obj) {
+    public EtapaObra create(EtapaObra obj) {
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
 
         if (Objects.isNull(userSpringSecurity) || !(userSpringSecurity.hasRole(ProfileEnum.ADMIN))) {
