fc1bb4a5055eb00c452bd2ac34c1b71e61137b69
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-12 17:52:41 -0300

    Add RestController at ObraController

--- backend/erp/src/main/java/com/coraduarte/erp/controllers/ObraController.java
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/ObraController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.coraduarte.erp.models.Obra;
@@ -18,6 +19,7 @@ import com.coraduarte.erp.services.ObraService;
 
 import jakarta.validation.Valid;
 
+@RestController
 public class ObraController {
    
     @Autowired
