0bd9bf87f8eb3bba160af75e036a0bdd38fa84cc
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-14 19:47:54 -0300

    Create ClienteController

---
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/ClienteController.java
@@ -0,0 +1,45 @@
+package com.coraduarte.erp.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.coraduarte.erp.models.Cliente;
+import com.coraduarte.erp.services.ClienteService;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity <Cliente> findById(@PathVariable Long id){
+        Cliente obj = this.clienteService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create (@Valid @RequestBody Cliente obj){
+        this.clienteService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> delete(@PathVariable Long id){
+        this.clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
