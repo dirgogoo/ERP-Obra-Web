3760acbb45229f14cef467e4194e2dc533929280
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-18 19:27:14 -0300

    Add annotations in EtapaController - Refactor

--- backend/erp/src/main/java/com/coraduarte/erp/controllers/EtapaController.java
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/EtapaController.java
@@ -4,11 +4,13 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,6 +20,8 @@ import com.coraduarte.erp.services.EtapaService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/etapa")
+@Validated
 public class EtapaController {
     
    @Autowired 
