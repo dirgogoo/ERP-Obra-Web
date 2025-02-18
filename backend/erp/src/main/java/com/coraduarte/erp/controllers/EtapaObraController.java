f8f57145735474591a71530d773adecfe2acad1a
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-18 17:22:33 -0300

    Add CRUD annotations in EtapaObraController - Refactor

--- backend/erp/src/main/java/com/coraduarte/erp/controllers/EtapaObraController.java
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/EtapaObraController.java
@@ -4,7 +4,10 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,17 +23,20 @@ public class EtapaObraController {
     @Autowired
     private EtapaObraService etapaObraService;
 
+    @GetMapping("/{id}")
     public ResponseEntity<EtapaObra> findById(@PathVariable Long id){
         EtapaObra obj = this.etapaObraService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody EtapaObra obj){
         this.etapaObraService.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         this.etapaObraService.delete(id);
         return ResponseEntity.noContent().build();
