945d90ca32db7b71b706e593a029df927a5d7ba8
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-02-18 19:29:24 -0300

    Add annotations in EtapaObraService - Refactor

--- backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coraduarte.erp.models.EtapaObra;
@@ -13,6 +14,7 @@ import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.EtapaObraRepository;
 import com.coraduarte.erp.security.UserSpringSecurity;
 
+@Service
 public class EtapaObraService {
 
     @Autowired
