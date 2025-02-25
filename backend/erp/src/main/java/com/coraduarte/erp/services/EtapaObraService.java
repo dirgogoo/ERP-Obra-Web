d1b0dee3e6ab5a50f9e993349124a83c5f45b3d6
Diego Oliveira <diego263526352635@gmail.com>
2025-02-25 15:33:44 -0300

    Added ObraLoader to EtapaObra

--- backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
@@ -6,17 +6,25 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coraduarte.erp.models.EtapaObra;
+import com.coraduarte.erp.models.Obra;
 import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.EtapaObraRepository;
+import com.coraduarte.erp.repositories.ObraRepository;
 import com.coraduarte.erp.security.UserSpringSecurity;
 
+@Service
 public class EtapaObraService {
 
     @Autowired
     private EtapaObraRepository etapaObraRepository;
+
+    @Autowired
+    
+    private ObraRepository ObraRepository;
 
     public EtapaObra findById(Long id) {
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
@@ -52,6 +60,9 @@ public class EtapaObraService {
             throw new AuthorizationDeniedException("Acesso negado!");
         }
 
+        Obra obra = this.ObraRepository.findById(obj.getObra().getId()).orElseThrow(() -> new RuntimeException("Obra não encontrada!"));
+        obj.setObra(obra);
+        
         obj.setId(null);
         obj = this.etapaObraRepository.save(obj);
         return obj;
