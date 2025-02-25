08d46a2218403e9a6f6f91629c2d9bf13abc4e11
Diego Oliveira <diego263526352635@gmail.com>
2025-02-25 09:49:25 -0300

    Added Pagination User

--- backend/erp/src/main/java/com/coraduarte/erp/services/UserService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.coraduarte.erp.models.User;
 import com.coraduarte.erp.models.enums.ProfileEnum;
 import com.coraduarte.erp.repositories.UserRepository;
