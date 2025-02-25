09daf945e711d01e27ed562344dc49ded6aefb25
Diego Oliveira <diego263526352635@gmail.com>
2025-02-25 15:30:43 -0300

    Updated CORS config

--- backend/erp/src/main/java/com/coraduarte/erp/config/WebConfig.java
+++ backend/erp/src/main/java/com/coraduarte/erp/config/WebConfig.java
@@ -7,8 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMapping(CorsRegistry registry){
-        registry.addMapping("/**");
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
 
 }
