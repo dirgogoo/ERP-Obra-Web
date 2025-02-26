ca699125d8bc4b61496a62da1aeb8c6b54cd44d4
Diego Oliveira <diego263526352635@gmail.com>
2025-02-26 12:29:41 -0300

    Created Cadastro Servicos System

--- frontend/src/components/TabelaServicos.vue
+++ frontend/src/components/TabelaServicos.vue
@@ -10,20 +10,15 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="servico in paginatedServicos" :key="servico.Id">
-                    <td>{{ servico.Id }}</td>
-                    <td>{{ servico.Nome }}</td>
-                    <td>{{ servico.Unidade }}</td>
-                    <td>{{ servico.Preço }}</td>
+                <tr v-for="servico in servicos" :key="servico.Id">
+                    <td>{{ servico.id }}</td>
+                    <td>{{ servico.nome }}</td>
+                    <td>{{ servico.unidade }}</td>
+                    <td>{{ servico.preço }}</td>
                 </tr>
             </tbody>
         </table>
-        <pagination
-            :data="servicos"
-            :limit="14"
-            @pagination-change-page="updatePage"
-        ></pagination>
-        <div v-if="servicos.length > perPage" id="selectionPage-container">
+        <div  id="selectionPage-container">
             <h1 @click="updatePage(currentPage - 1)">&lt;</h1>
             <h1 id="page-label">{{currentPage}}</h1>
             <div @click="updatePage(currentPage + 1)"><h1>&gt;</h1> </div>
@@ -32,48 +27,66 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue';
-import Pagination from 'laravel-vue-pagination';
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
+import api from "../services/axios";
 
 export default {
     name: 'TabelaServicos',
-    components: {
-        Pagination
-    },
     setup() {
-        const servicos = ref([
-            { Id: 1, Nome: 'Serviço 1', Unidade: 'kg', Preço: 10.0 },
-            { Id: 2, Nome: 'Serviço 2', Unidade: 'm', Preço: 20.0 },
-            { Id: 3, Nome: 'Serviço 3', Unidade: 'l', Preço: 30.0 },
-            { Id: 4, Nome: 'Serviço 4', Unidade: 'kg', Preço: 40.0 },
-            { Id: 5, Nome: 'Serviço 5', Unidade: 'm', Preço: 50.0 },
-            { Id: 6, Nome: 'Serviço 6', Unidade: 'l', Preço: 60.0 },
-            { Id: 7, Nome: 'Serviço 7', Unidade: 'kg', Preço: 70.0 },
-            { Id: 8, Nome: 'Serviço 8', Unidade: 'm', Preço: 80.0 },
-            { Id: 9, Nome: 'Serviço 9', Unidade: 'l', Preço: 90.0 },
-            { Id: 10, Nome: 'Serviço 10', Unidade: 'kg', Preço: 100.0 },
-            // Adicione mais serviços conforme necessário
-        ]);
+        const servicos = ref([]);
         const currentPage = ref(1);
-        const perPage = ref(14);
+        const perPage = ref(16);
 
-        const paginatedServicos = computed(() => {
-            const start = (currentPage.value - 1) * perPage.value;
-            const end = start + perPage.value;
-            return servicos.value.slice(start, end);
-        });
+        const fetchServicos = async (page) => {
+            try {
+                const response = await api.get('/item', {
+                    params: {
+                        type: 1,
+                        page: page - 1,
+                        size: perPage.value
+                    }
+                });
+                
+                servicos.value = response.data.content.map(servico => ({
+                    id: servico.id,
+                    nome: servico.name,
+                    unidade:servico.unidade,
+                    preço :servico.valor
+                }));
 
-        const updatePage = (page) => {
-            if (page >= 1 && page <= Math.ceil(servicos.value.length / perPage.value)){
                 currentPage.value = page;
+
+            } catch (error) {
+                console.error("Erro ao buscar servicos:", error);
             }
         };
+
+        
+
+        const handleUserRegistered = () => {
+            fetchServicos(currentPage.value);
+        };
+
+        onMounted(() => {
+            fetchServicos(currentPage.value);
+            window.addEventListener('servico-registered', handleUserRegistered);
+        });
+
+        onBeforeUnmount(() => {
+            window.removeEventListener('servico-registered', handleUserRegistered);
+        });
+
+        const updatePage = (page) => { {
+            if (page > 0){
+                fetchServicos(page);
+            }
+                
+        }};
 
         return {
             servicos,
             currentPage,
             perPage,
-            paginatedServicos,
             updatePage
         };
     }
