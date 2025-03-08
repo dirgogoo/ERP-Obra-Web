eb86a0504638e5b525838e65c8a22fcf64d892b4
Diego Oliveira <diego263526352635@gmail.com>
2025-03-08 01:48:04 -0300

    Created Load etapaObra table

--- frontend/src/components/TabelaEtapaObra.vue
+++ frontend/src/components/TabelaEtapaObra.vue
@@ -16,15 +16,15 @@
         </thead>
         <tbody>
           <tr v-for="etapa in paginatedEtapas" :key="etapa.Id" @click="toRouteId(etapa.Id)">
-            <td>{{ etapa.Id }}</td>
-            <td>{{ etapa.Nome }}</td>
-            <td>R${{ etapa.ValorTotal }}</td>
-            <td>R${{ etapa.Despesas }}</td>
-            <td>R${{ etapa.Saldo }}</td>
-            <td>{{ etapa.Inicio }}</td>
-            <td>{{ etapa.Final }}</td>
-            <td>{{ etapa.DiasRes }}</td>
-            <td>{{ etapa.Status }}</td>
+            <td>{{ etapa.id }}</td>
+            <td>{{ etapa.etapa.name }}</td>
+            <td>R${{ etapa.price }}</td>
+            <td>R$0</td>
+            <td>R${{ etapa.price }}</td>
+            <td>-</td>
+            <td>{{ etapa.deadline }}</td>
+            <td>-</td>
+            <td>{{ etapa.status }}</td>
           </tr>
         </tbody>
       </table>
@@ -41,63 +41,36 @@
     </div>
   </template>
   
-  <script>
+  <script setup>
   import { ref, computed } from 'vue';
   import Pagination from 'laravel-vue-pagination';
   import { useRouter } from 'vue-router';
   
-  export default {
-    name: 'TabelaEtapa',
-    components: {
-      Pagination
-    },
-    setup() {
-      const router = useRouter();
-      const etapas = ref([
-        { Id: 1, Nome: 'Etapa 1', ValorTotal: 1000, Despesas: 200, Saldo: 800, Inicio: '2023-01-01', Final: '2023-01-10', DiasRes: 9, Status: 'Em andamento' },
-        { Id: 2, Nome: 'Etapa 2', ValorTotal: 1500, Despesas: 500, Saldo: 1000, Inicio: '2023-02-01', Final: '2023-02-15', DiasRes: 14, Status: 'Não Iniciado' }, 
-        { Id: 3, Nome: 'Etapa 3', ValorTotal: 2000, Despesas: 800, Saldo: 1200, Inicio: '2023-03-01', Final: '2023-03-20', DiasRes: 19, Status: 'Concluído' },
-      { Id: 4, Nome: 'Etapa 4', ValorTotal: 2500, Despesas: 1000, Saldo: 1500, Inicio: '2023-04-01', Final: '2023-04-25', DiasRes: 24, Status: 'Atrasado' },
-      { Id: 5, Nome: 'Etapa 5', ValorTotal: 3000, Despesas: 1200, Saldo: 1800, Inicio: '2023-05-01', Final: '2023-05-30', DiasRes: 29, Status: 'Em andamento' },
-      { Id: 6, Nome: 'Etapa 6', ValorTotal: 3500, Despesas: 1500, Saldo: 2000, Inicio: '2023-06-01', Final: '2023-06-20', DiasRes: 19, Status: 'Não Iniciado' },
-      { Id: 7, Nome: 'Etapa 7', ValorTotal: 4000, Despesas: 1800, Saldo: 2200, Inicio: '2023-07-01', Final: '2023-07-25', DiasRes: 24, Status: 'Concluído' },
-      { Id: 8, Nome: 'Etapa 8', ValorTotal: 4500, Despesas: 2000, Saldo: 2500, Inicio: '2023-08-01', Final: '2023-08-30', DiasRes: 29, Status: 'Atrasado' },
-      { Id: 9, Nome: 'Etapa 9', ValorTotal: 5000, Despesas: 2200, Saldo: 2800, Inicio: '2023-09-01', Final: '2023-09-20', DiasRes: 19, Status: 'Em andamento' },
-      { Id: 10, Nome: 'Etapa 10', ValorTotal: 5500, Despesas: 2500, Saldo: 3000, Inicio: '2023-10-01', Final: '2023-10-25', DiasRes: 24, Status: 'Não Iniciado' },
-      { Id: 11, Nome: 'Etapa 11', ValorTotal: 6000, Despesas: 2800, Saldo: 3200, Inicio: '2023-11-01', Final: '2023-11-30', DiasRes: 29, Status: 'Concluído' },
-      { Id: 12, Nome: 'Etapa 12', ValorTotal: 6500, Despesas: 3000, Saldo: 3500, Inicio: '2023-12-01', Final: '2023-12-20', DiasRes: 19, Status: 'Atrasado' },
-      { Id: 13, Nome: 'Etapa 13', ValorTotal: 7000, Despesas: 3200, Saldo: 3800, Inicio: '2024-01-01', Final: '2024-01-25', DiasRes: 24, Status: 'Em andamento' },
-      { Id: 14, Nome: 'Etapa 14', ValorTotal: 7500, Despesas: 3500, Saldo: 4000, Inicio: '2024-02-01', Final: '2024-02-15', DiasRes: 14, Status: 'Não Iniciado' },
-        // Adicione mais etapas conforme necessário
-      ]);
-      const currentPage = ref(1);
-      const itemsPerPage = 15;
-  
-      const paginatedEtapas = computed(() => {
-        const start = (currentPage.value - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
-        return etapas.value.slice(start, end);
-      });
-  
-      function updatePage(page) {
-        if (page >= 1 && page <= Math.ceil(etapas.value.length / perPage.value)){
-                currentPage.value = page;
-            }
-      }
-  
-      const toRouteId = (id) => {
-        //router.push(`/etapa/${id}`);
-      };
-  
-      return {
-        etapas,
-        currentPage,
-        itemsPerPage,
-        paginatedEtapas,
-        updatePage,
-        toRouteId
-      };
+  const props = defineProps({
+    etapas: {
+      type: Array,
+      required: true
     }
+  });
+  
+  const router = useRouter();
+  const currentPage = ref(1);
+  const itemsPerPage = 15;
+  
+  const paginatedEtapas = computed(() => {
+    const start = (currentPage.value - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    return props.etapas.slice(start, end);
+  });
+  
+  function updatePage(page) {
+    if (page >= 1 && page <= Math.ceil(props.etapas.length / itemsPerPage)) {
+      currentPage.value = page;
+    }
+  }
+  
+  const toRouteId = (id) => {
+   // router.push(`/etapa/${id}`);
   };
   </script>
 
