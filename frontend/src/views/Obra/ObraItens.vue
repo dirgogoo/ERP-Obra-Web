99b62f20b5782eac8c3bbb11393205d9988f5804
Diego Oliveira <diego263526352635@gmail.com>
2025-03-12 20:36:14 -0300

    ObraItens show ItensEtapas by EtapaObra

--- frontend/src/views/Obra/ObraItens.vue
+++ frontend/src/views/Obra/ObraItens.vue
@@ -1,61 +1,94 @@
 <script setup>
-    import FilterSelector from '@/components/FilterSelector.vue';
-    import TabelaItemObra from '@/components/TabelaItemObra.vue';
-    import Button from '@/components/Button.vue';
-    import { useRouter } from 'vue-router';
+import FilterSelector from '@/components/FilterSelector.vue';
+import TabelaItemObra from '@/components/TabelaItemObra.vue';
+import Button from '@/components/Button.vue';
+import { useRouter } from 'vue-router';
 
-    const route = useRouter();
+import { onBeforeMount, ref, watch } from 'vue';
+const props = defineProps({
+    obra: {
+        type: Object,
+    }
+});
 
-    const currentPath = route.currentRoute.value.params.id;
+const etapasSelecao = ref([]);
+const etapa = ref();
+const etapaObj = ref(0);
 
-  const toRouteId = (item) => {
-    route.replace("/app/obra/"+currentPath+ '/itens/' +item.toLowerCase());
-  };
+const itens = ref([]);
+
+const route = useRouter();
+
+const currentPath = route.currentRoute.value.params.id;
+
+onBeforeMount(() => {
+    
+    for (let i = 0; i < props.obra.etapa.length; i++) {
+        etapasSelecao.value.push(props.obra.etapa[i].etapa.name);
+    }
+});
+
+const toRouteId = (item) => {
+    route.replace("/app/obra/" + currentPath + '/itens/' + item.toLowerCase());
+
+};
+
+
+watch(etapa, (newVal) => {
+    const foundedEtapaObra = props.obra.etapa.find((etapa) => etapa.etapa.name === newVal);
+    if (foundedEtapaObra) {
+        etapaObj.value = foundedEtapaObra.id;
+        console.log(etapaObj.value);
+        itens.value = foundedEtapaObra.itens;
+    }
+});
+
 </script>
 
 <template>
     <div id="main-container">
         <div id="form-container">
-            <FilterSelector class="FilterSelector" label="Etapa Itens:"/>
-            <FilterSelector class="FilterSelector"  label="Ordenar por:"/>
-            <Button id="button" label="Gerenciar Itens" @click="toRouteId('gerenciar')"/>
+            <FilterSelector class="FilterSelector" label="Etapa Itens:" :content="etapasSelecao" v-model="etapa"/>
+            <FilterSelector class="FilterSelector" label="Ordenar por:" />
+            <Button id="button" label="Gerenciar Itens" @click="toRouteId(`gerenciar?etapaObra=${etapaObj}`)" />
         </div>
         <div id="table-container">
-            <TabelaItemObra/>
+            <TabelaItemObra :items="itens" />
         </div>
     </div>
 </template>
 
 <style scoped>
-    #main-container{
-        height: 100%;
-        width: 100%;
-        margin-left: 0.5%;
-    }
-    #form-container{
-        height: 10%;
-        width: 98%;
-        display: flex;
-        justify-content: center;
-        font-size: 0.9em;
-        gap: 2%;
-    }
+#main-container {
+    height: 100%;
+    width: 100%;
+    margin-left: 0.5%;
+}
 
-    #button{
-        margin-left: 8%;
-        font-size: 1.7em;;
-        height: 60px;
-        width: 60%;
-    }
+#form-container {
+    height: 10%;
+    width: 98%;
+    display: flex;
+    justify-content: center;
+    font-size: 0.9em;
+    gap: 2%;
+}
 
-    .FilterSelector{
-        margin-top: 10px;
-    }
+#button {
+    margin-left: 8%;
+    font-size: 1.7em;
+    ;
+    height: 60px;
+    width: 60%;
+}
 
-    #table-container{
-        margin-top: 20px;
-        height: 85%;
-        width: 98%;
-    }
+.FilterSelector {
+    margin-top: 10px;
+}
 
+#table-container {
+    margin-top: 20px;
+    height: 85%;
+    width: 98%;
+}
 </style>
