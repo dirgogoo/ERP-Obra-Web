f0609d8660ce46f120344c646a0316fce4e5b5ea
Diego Oliveira <diego263526352635@gmail.com>
2025-03-17 21:58:28 -0300

    Added FindSaldoByID EtapaObra

--- backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/EtapaObraService.java
@@ -5,12 +5,14 @@ import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coraduarte.erp.models.EtapaObra;
 import com.coraduarte.erp.models.enums.ProfileEnum;
+import com.coraduarte.erp.models.itemEtapa;
 import com.coraduarte.erp.repositories.EtapaObraRepository;
 import com.coraduarte.erp.security.UserSpringSecurity;
 
@@ -19,6 +21,10 @@ public class EtapaObraService {
 
     @Autowired
     private EtapaObraRepository etapaObraRepository;
+
+    @Autowired
+    @Lazy
+    private ItemEtapaService itemEtapaService;
 
 
     public EtapaObra findById(Long id) {
@@ -33,6 +39,28 @@ public class EtapaObraService {
                 "Etapa não encontrada!"
         ));
     }
+
+    public Double findSaldoById(Long id) {
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        if (Objects.isNull(userSpringSecurity)) {
+            throw new AuthorizationDeniedException("Acesso negado!");
+        }
+
+        Optional<EtapaObra> etapa = this.etapaObraRepository.findById(id);
+        List<itemEtapa> items = this.itemEtapaService.findAllbyEtapaObraIdAll(id);
+
+        Double saldo = 0.0;
+        for (itemEtapa item : items) {
+            if (Objects.nonNull(item)) {
+                saldo += item.getQuantidade()*item.getItem().getValor();
+            }
+        }
+
+        saldo = etapa.get().getPrice() - saldo;
+        return saldo;
+    }
+
     
 
     public List<EtapaObra> findAllByObraId(Long id){
@@ -47,6 +75,8 @@ public class EtapaObraService {
 
 
     }
+
+    
 
     @Transactional
     public EtapaObra create(EtapaObra obj) {
