c0ba3c5de2cc946701f2718cdf473be43fa8ff7c
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-03-24 17:42:24 -0300

    Added EtapaObraService class

--- backend/erp/src/main/java/com/coraduarte/erp/services/ItemEtapaService.java
+++ backend/erp/src/main/java/com/coraduarte/erp/services/ItemEtapaService.java
@@ -13,6 +13,7 @@ import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.coraduarte.erp.services.EtapaObraService;
 import com.coraduarte.erp.models.EtapaObra;
 import com.coraduarte.erp.models.Item;
 import com.coraduarte.erp.models.enums.ProfileEnum;
