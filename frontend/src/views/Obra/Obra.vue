50c32349bbabd6a19c26e235a29e56bcc4cd572d
Diego Oliveira <diego263526352635@gmail.com>
2025-03-24 22:05:25 -0300

    Updated ObraCriar

--- frontend/src/views/Obra/Obra.vue
+++ frontend/src/views/Obra/Obra.vue
@@ -15,6 +15,7 @@
         <div id="top-container">
             <div id="searchbar-container">
                  <searchbar v-model="search"/>
+                 
             </div>
             <div class="filter-container">
                 <FilterSelector label="Ordenar por:"/>
