cb1f5838a8a45c507a6de07624d7283e945d8b73
Diego Oliveira <diego263526352635@gmail.com>
2025-04-01 22:15:13 -0300

    Load on Mounted updated dias ObraGeral

--- frontend/src/views/Obra/ObraGeral.vue
+++ frontend/src/views/Obra/ObraGeral.vue
@@ -22,38 +22,45 @@ const dataInicio = props.obra.dataInicio;
 
 const dataHoje = new Date();
 
+const getDataAtual = (data1) => {
+    console.log(data1)
+    const [day1, month1, year1] = data1.split('/');
+    const data1N = `${year1}-${month1}-${day1}`;
+    const data1Date = new Date(data1N);
+    return data1Date;
+}
+
 const subtrairDatas = (data1, data2) => {
-  console.log(data1, data2)
-    //11/03/2024 para 2024-03-11
-  const [day1, month1, year1] = data1.split('/');
-  const [day2, month2, year2] = data2.split('/');
-  const data1N = `${year1}-${month1}-${day1}`;
-  const data2N = `${year2}-${month2}-${day2}`;
+    console.log(data1, data2)
 
-// Converte as strings de data para objetos Date
-  const data1Date = new Date(data1N);
-  const data2Date = new Date(data2N);
-  
-  const diffTime = data1Date - data2Date;
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    // Converte as strings de data para objetos Date
+    const data1Date = getDataAtual(data1);
+    const data2Date = getDataAtual(data2);
 
-  return diffDays;
+    const diffTime = data1Date - data2Date;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    return diffDays;
 }
 
 
 
-const diasPassados = ref(Math.abs(subtrairDatas(dataInicio ,dataHoje.toLocaleDateString('pt-BR'))));
+const diasPassados = ref(0);
 
-console.log(dataHoje.toLocaleDateString('pt-BR'))
-const diasRestantes = ref(subtrairDatas( prazo.value,dataHoje.toLocaleDateString('pt-BR')));
+
+const diasRestantes = ref(0);
+const diaHoje = dataHoje.toLocaleDateString('pt-BR');
 
 
 console.log(props.obra);
 
-const dataPie = ref([0,0,0,0])
+const dataPie = ref([0, 0, 0, 0])
 
 
 onMounted(async () => {
+    diasPassados.value = Math.max(0, subtrairDatas(diaHoje, dataInicio));
+
+    diasRestantes.value = subtrairDatas(prazo.value, diaHoje);
     var obra = props.obra;
     var saldoObra = 0;
     var obraTotal = 0
@@ -63,7 +70,7 @@ onMounted(async () => {
             saldoObra += response.data;
             obraTotal += obra.etapa[i].price;
 
-            switch(obra.etapa[i].status){
+            switch (obra.etapa[i].status) {
                 case "ANDAMENTO":
                     dataPie.value[0]++;
                     break;
@@ -82,7 +89,7 @@ onMounted(async () => {
         } catch (error) {
             console.error("Erro ao buscar itens:", error);
         }
-        
+
     }
     console.log(dataPie.value);
     saldoObraV.value = saldoObra;
@@ -109,19 +116,19 @@ onMounted(async () => {
 
                 </div>
                 <div class="box-container">
-                    <BoxInfo :color=blueColor label="Gastos Realizados" :info="obraTotalV-saldoObraV" />
+                    <BoxInfo :color=blueColor label="Gastos Realizados" :info="obraTotalV - saldoObraV" />
                 </div>
                 <div class="box-container">
                     <BoxInfo label="Dias Restantes" :info="diasRestantes" />
                 </div>
                 <div class="box-container">
-                    <BoxInfo  label="Saldo" :info="saldoObraV" />
+                    <BoxInfo label="Saldo" :info="saldoObraV" />
                 </div>
             </div>
         </div>
         <div id="right-container">
             <h2>Progresso das Etapas:</h2>
-            <ObraChart :dataPie = "dataPie" />
+            <ObraChart :dataPie="dataPie" />
         </div>
     </div>
 
