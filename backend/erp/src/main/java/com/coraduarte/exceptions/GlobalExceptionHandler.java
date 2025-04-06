cbc25910b429e3dbed6d024a20baee29bf629aa6
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-04-06 16:04:51 -0300

    Refactor GlobalExcepectionHandler

--- backend/erp/src/main/java/com/coraduarte/exceptions/GlobalExceptionHandler.java
+++ backend/erp/src/main/java/com/coraduarte/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.coraduarte.exceptions;
 
 
 
+import java.io.IOException;
+
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -10,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,12 +25,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.coraduarte.erp.services.exceptions.DataBindingViolationException;
 import com.coraduarte.erp.services.exceptions.ObjectNotFoundException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler implements AuthenticationFailureHandler{
     
     @Value("$(server.error.include-exception)")
     private boolean printStackTrace;
@@ -128,6 +135,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
             errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
         }
         return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException exception) throws IOException, ServletException {
+          Integer status = HttpStatus.FORBIDDEN.value();
+          response.setStatus(status);
+          response.setContentType("application/json");
+          ErrorResponse errorResponse = new ErrorResponse(status, "Email ou senha inválidos");
+          response.getWriter().append(errorResponse.toJson());
     }
 
 }
