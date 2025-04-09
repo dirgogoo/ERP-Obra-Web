f932f35792366c8b61448279205a6c2223878563
Diego Oliveira <diego263526352635@gmail.com>
2025-04-09 18:31:06 -0300

    Added Update ItemEtapa backend

--- backend/erp/src/main/java/com/coraduarte/erp/controllers/ItemEtapaController.java
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/ItemEtapaController.java
@@ -1,6 +1,7 @@
 package com.coraduarte.erp.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.coraduarte.erp.models.ItemEtapa;
+import com.coraduarte.erp.models.Obra;
 import com.coraduarte.erp.services.ItemEtapaService;
 
 import jakarta.validation.Valid;
@@ -33,9 +36,16 @@ public class ItemEtapaController {
     }
 
     @GetMapping("/EtapaObra/{id}")
-    public ResponseEntity <Page<ItemEtapa>> findByEtapaObraId(@PathVariable Long id, Pageable pageable){
-        Page<ItemEtapa> obj = this.itemEtapaService.findAllbyEtapaObraId(id, pageable);
+    public ResponseEntity <List<ItemEtapa>> findByEtapaObraId(@PathVariable Long id){
+        List<ItemEtapa> obj = this.itemEtapaService.findAllbyEtapaObraIdAll(id);
        return ResponseEntity.ok().body(obj);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update( @RequestBody ItemEtapa obj) {
+        this.itemEtapaService.update(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @PostMapping
