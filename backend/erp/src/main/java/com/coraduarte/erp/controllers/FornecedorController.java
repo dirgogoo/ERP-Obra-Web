2e4c4d535aec4ad2b1ee0a7dcd2dc88c7fac29c9
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-04-20 16:23:38 -0300

    Created FornecedorController

---
+++ backend/erp/src/main/java/com/coraduarte/erp/controllers/FornecedorController.java
@@ -0,0 +1,44 @@
+package com.coraduarte.erp.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.coraduarte.erp.services.FornecedorService;
+
+import jakarta.validation.Valid;
+
+import com.coraduarte.erp.models.Fornecedor;
+
+
+@RestController
+@RequestMapping("/fornecedor")
+@Validated
+public class FornecedorController {
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> findById(@PathVariable Long id){
+       Fornecedor obj = this.fornecedorService.findById(id);
+       return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create (@Valid @RequestBody Fornecedor obj){
+        this.fornecedorService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    
+}
