a969fde7d519fe30d0b6a520b9cb424819b9a9e8
Julia Coelho <juliaalvescoelho82@gmail.com>
2025-04-20 16:22:32 -0300

    Created FornecedorRepository

---
+++ backend/erp/src/main/java/com/coraduarte/erp/repositories/FornecedorRepository.java
@@ -0,0 +1,13 @@
+package com.coraduarte.erp.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coraduarte.erp.models.Fornecedor;
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+    Page<Fornecedor> findByNameContainingIgnoreCase(String nome, Pageable pageable);
+}
